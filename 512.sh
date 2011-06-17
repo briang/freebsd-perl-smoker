c0098bcf4406ccf5ba8ccaaa7e371ee97c20fb4b
brian greenfield <briang@cpan.org>
2011-06-17 14:19:02 +0100

    add pidfile to 512 configuration

--- 512.sh
+++ 512.sh
@@ -12,18 +12,18 @@
 cd /usr/home/bri/smoke
 CFGNAME=${CFGNAME:-512_config}
 LOCKFILE=${LOCKFILE:-512.lck}
+PIDFILE=${PIDFILE:-512.pid}
 continue=''
 if test -f "$LOCKFILE" && test -s "$LOCKFILE" ; then
     echo "We seem to be running (or remove $LOCKFILE)" >& 2
     exit 200
-
 fi
 echo "$CFGNAME" > "$LOCKFILE"
-
+echo "$$"       > "$PIDFILE"
 
 PATH=.:/home/bri/perl5/perlbrew/bin:/home/bri/perl5/perlbrew/perls/perl-5.12.3/bin:~/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/games:/usr/local/sbin:/usr/local/bin
 export PATH
 umask 0
 /home/bri/perl5/perlbrew/perls/perl-5.12.3/bin/perl ./smokeperl.pl -c "$CFGNAME" $continue $* > 512.log 2>&1
 
-rm "$LOCKFILE"
+rm "$LOCKFILE" "$PIDFILE"
