1c13ab49712b6d220ccbe673331df9383fbe7900
brian greenfield <briang@cpan.org>
2011-06-17 15:42:25 +0100

    add 5.14 configuration

---
+++ 514.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+#
+# Written by ./configsmoke.pl v0.072
+# Thu Jun 16 21:27:22 2011
+# NOTE: Changes made in this file will be *lost*
+#       after rerunning ./configsmoke.pl
+#
+# 
+# Uncomment this to be as nice as possible. (Jarkko)
+# (renice -n 20 $$ >/dev/null 2>&1) || (renice 20 $$ >/dev/null 2>&1)
+
+cd /usr/home/bri/smoke
+CFGNAME=${CFGNAME:-514_config}
+LOCKFILE=${LOCKFILE:-514.lck}
+PIDFILE=${PIDFILE:-514.pid}
+continue=''
+if test -f "$LOCKFILE" && test -s "$LOCKFILE" ; then
+    echo "We seem to be running (or remove $LOCKFILE)" >& 2
+    exit 200
+fi
+echo "$CFGNAME" > "$LOCKFILE"
+echo "$$"       > "$PIDFILE"
+
+PATH=.:/home/bri/perl5/perlbrew/bin:/home/bri/perl5/perlbrew/perls/perl-5.12.3/bin:~/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/games:/usr/local/sbin:/usr/local/bin
+export PATH
+umask 0
+/home/bri/perl5/perlbrew/perls/perl-5.12.3/bin/perl ./smokeperl.pl -c "$CFGNAME" $continue $* > 514.log 2>&1
+
+rm "$LOCKFILE" "$PIDFILE"
